650fa5bd208c0c9e2ae6b89c94d9a0b6cef54bbb
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2024-10-17 19:22:49 +0800

    Created simple HBS starter template

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const hbs = require("hbs");
 
-
+/**
+ * TEMPLATE ENGINE
+ */
+app.set("view engine", "hbs")
 
 /**
  * ROUTES
  */
-app.get("/",function(){
-    res.send("HElo");
+app.get("/",function(req,res){
+    res.render("hello");
 })
 
-app.listen(3000, function(){
+app.listen(3000, function(req,res){
     console.log("Server is running");
 })
